0b3580979c13b130e9e03fa841cd45a2fd7809b3
pavlovskaya.v <Vera.Pavlovskaya2008@yandex.ru>
2022-12-05 16:39:13 +0300

    Добавили функцию render

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ class Board:
         self.cell_size = 30
 
     def render(self, screen):
-        pass
+        for x in range(self.width):
+            for y in range(self.height):
+                pygame.draw.rect(screen, pygame.Color(255, 255, 255),
+                                 (x * self.cell_size + self.left,
+                                  y * self.cell_size + self.top,
+                                  self.cell_size, self.cell_size), 1)
 
 
 def main():
